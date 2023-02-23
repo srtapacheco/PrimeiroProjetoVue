43cb41f94d4ddc15d42b224018093a83fd7d7007
srtapacheco <karenpacheco16@poli.ufrj.br>
2023-02-23 04:09:29 -0300

    Inicializando um segundo modulo para estruturar a componentizacao do vue

---
+++ 2_projeto_vue/src/components/PrimeiroComponente.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1> Olá vue </h1>
+</template>
+
+<script>
+//exportando para o mundo o componente, através da nomeacao do mesmo, é possivel acessá-lo em outros lugares
+export default {
+  name: 'PrimeiroComponente'
+}
+</script>
