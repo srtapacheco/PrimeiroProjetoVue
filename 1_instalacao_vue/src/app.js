c7d232754a67c46f6514796a96b7a63b1897c749
srtapacheco <karenpacheco16@poli.ufrj.br>
2023-02-23 02:11:41 -0300

    Finalizando a construcao de um formulario simples via CDN

--- 1_instalacao_vue/src/app.js
+++ 1_instalacao_vue/src/app.js
@@ -2,9 +2,21 @@
 const MeuNomeApp = {
     data() {
         return {
-            nome: "Karen",
+            nome: "",
             idade: 23,
+            campoNome: ""
         }
+    },
+
+    //funcoes da aplicacao
+    methods :{
+        //e resgata a acao do evento
+        enviaFormulario (e){
+            e.preventDefault(); //impede que o formulario seja enviado e pagina recarregue
+            this.nome = this.campoNome;
+            return false;
+        }
+
     }
 }
 
