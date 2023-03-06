05a95376d5f1cfbba5a14b0a2a6ccb2aa973fd08
srtapacheco <karenpacheco16@poli.ufrj.br>
2023-03-06 01:21:28 -0300

    Aula 10 - Explorando atributos dinamicos do Vue

--- 2_projeto_vue/src/components/Info.vue
+++ 2_projeto_vue/src/components/Info.vue
@@ -1,24 +1,36 @@
+
+
 <template>
   <div>
-    <p> Estou trabalhando no momento.</p>
+    <p v-if="estaTrabalhando"> Estou trabalhando no momento.</p>
+    <p v-else> Nao estou trabalhando no momento.</p>
     <p> Utilizo as seguintes tecnologias: </p>
     <ul>
       <li>JavaScript</li>
       <li>Python</li>
       <li>C</li>
       <li>Dart</li>
-
     </ul>
+    <p v-show="mostrarEmail"> Mande uma mensagem para: {{email}}</p>
+    <!-- v-bind serve pra url dinamicas -->
+    <p> Acesse o portifolio <a v-bind:href="meu_link">aqui</a> </p>
+    <Picture> </Picture>
   </div>
 </template>
 
 <script>
-export default {
-  name:'Info',
-  data(){
-    return{
+import Picture from './Picture.vue';
 
-    }
-  }
+export default {
+    name: "Info",
+    data() {
+        return {
+            estaTrabalhando: false,
+            mostrarEmail: false,
+            email: "karenpacheco16@poli.ufrj.br",
+            meu_link: "https://htmlcolorcodes.com/"
+        };
+    },
+    components: { Picture }
 }
 </script>
---
+++ 2_projeto_vue/src/components/Picture.vue
@@ -0,0 +1,15 @@
+<template>
+    <img v-bind:src="avatar" v-bind:alt="descricao">
+</template>
+
+<script>
+export default{
+    name: 'Picture',
+    data (){
+        return{
+            avatar: "/img/avatar.png" ,
+            descricao: "foto de perfil do usuario" 
+        }
+    }
+}
+</script>
